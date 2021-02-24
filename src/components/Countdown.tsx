8a6ca86887d99fc5c282b1048c7915508b56f476
Gabriel Nunes <prismaticnetwork12@gmail.com>
2021-02-24 18:32:07 -0300

    [feat] criação e modernização do componente countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css';
+
+let countdownTimeout: NodeJS.Timeout;
+
 export function Countdown() {
   const [time, setTime] = useState(30 * 60);
   const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -14,11 +18,20 @@ export function Countdown() {
     setIsActive(true);
   }
 
+  function resetCountdown() {
+    setIsActive(false);
+    clearTimeout(countdownTimeout);
+    setTime(30 * 60);
+  }
+
   useEffect(() => {
     if(isActive && time > 0) {
-      setTimeout(() => {
+      countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000)
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
     }
   }, [isActive, time])
 
@@ -35,12 +48,35 @@ export function Countdown() {
           <span>{secondRight}</span>
         </div>
       </div>
-      <button
-        onClick={startCountdown}
-        type="button" 
-        className={styles.countdownButton}>
-        Iniciar
-      </button>
+
+      {hasFinished ? (
+        <button
+          disabled
+          className={styles.countdownButton}
+        >
+          Cíclo encerrado
+        </button>
+      ) : (
+        <>
+          {isActive ? (
+            <button
+              onClick={resetCountdown}
+              type="button" 
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+            >
+              Abandonar cíclo
+            </button>
+          ) : (
+            <button
+              onClick={startCountdown}
+              type="button" 
+              className={styles.countdownButton}
+            >
+              Iniciar um Cíclo
+            </button>
+          )}
+        </>
+      )}
     </div>
 
   );
